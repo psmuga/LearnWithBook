3d16d01207f5200e75ed068ba470f973b180218b
Piotr Smuga <smuga.piotr@protonmail.com>
2017-01-31 00:30:02 +0100

    klasa word | enumerator |enumerable | iterator

--- Chapter4_2/Program.cs
+++ Chapter4_2/Program.cs
@@ -1,24 +1,166 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
 
 namespace Chapter4_2
 {
     class Program
     {
+        delegate int Dziel(int i);
         static void Main(string[] args)
         {
 
             try
             {
-
+                Dziel abc = x => 10 / x;
+                abc(0);
+                Console.WriteLine("Linia nastepna po  błędzie"); // nie  wyswiwtli 
             }
-            catch (Exception e)
+            catch (DivideByZeroException e) //when (e.    ) filtr wyjatku
             {
-                Console.WriteLine(e);
-                throw;
+                Console.WriteLine("blad ");
+                //throw;
+            }
+            catch 
+            {
+                Console.WriteLine("every other exceptions");
+            }
+
+
+           // File.Create("file.txt");
+            StreamReader reader = null;
+            try
+            {
+                reader = File.OpenText("file.txt");
+                if (reader.EndOfStream) return;
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            finally
+            {
+                //if (reader != null) reader.Dispose();
+                reader?.Dispose(); //to samo
+            }
+
+            using (var cosStreamReader = File.OpenText("File.txt")) //to jest równoważne z całym blokkiem try wyzej razem z stworzeniem readera
+            {
+                Console.WriteLine(cosStreamReader.ReadToEnd());
+            }
+
+            //wzorzec metod tryXXX str 165
+
+            var dic = new Dictionary<int, string>()
+            {
+                {1,"ala" },
+                {2,"ma" }
+            };
+            var dict = new Dictionary<int,string>()
+            {
+                [5] = "five",
+                [10] = "ten"
+            };
+
+
+
+            foreach (var x in Fibs(8))
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine();  
+            foreach (var x in ParzysteFib(Fibs(8)))
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine();
+
+
+            foreach (var x in GetSequence(4,10))
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine();
+            var cos = (IEnumerator<int>) GetSequence(2, 8).GetEnumerator();
+            while (cos.MoveNext())
+            {
+                Console.Write(cos.Current + " ");
+            }
+            Console.WriteLine();
+
+            var test = new Word();
+            Console.WriteLine(test[1]);
+            foreach (var VARIABLE in Word.Zdanie())
+            {
+                Console.Write(VARIABLE + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+
+
+
+        class Word
+        {
+            private static string[] _w = "Ala ma kota a kot ma ale".Split();
+
+            public string this[int nr]
+            {
+                get { return _w[nr]; }
+                set { _w[nr] = value; }
+            }
+
+            public static IEnumerable<string> Zdanie()
+            {
+                foreach (var VARIABLE in _w)
+                {
+                    yield return VARIABLE;
+                }
+            }
+        }
+
+
+
+
+
+
+        static IEnumerable<int> Fibs(int x)
+        {
+            for (int i = 0, prev =1,cur =1; i < x; i++)
+            {
+                yield return prev;
+                int newFib = prev + cur;
+                prev = cur;
+                cur = newFib;
+            }
+        }
+
+        static IEnumerable<int> ParzysteFib(IEnumerable<int> sequence) //komponowaaanie iteratorów
+        {
+            foreach (var VARIABLE in sequence)
+            {
+                if ((VARIABLE % 2) == 0) yield return VARIABLE;
+            }
+        }
+
+
+
+        static IEnumerable<int> GetSequence(int fromValue, int toValue)
+        {
+            if (toValue >= fromValue)
+            {
+                for (int i = fromValue; i <= toValue; i++)
+                {
+                    yield return i;
+                }
+            }
+            else
+            {
+                for (int i = fromValue; i >= toValue; i--)
+                {
+                    yield return i;
+                }
             }
         }
     }
