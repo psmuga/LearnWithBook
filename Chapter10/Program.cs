c8db427e40c256ea6d2fcab3fe218525fd8dee79
piotr <piotr@DESKTOP-19A5Q6V>
2017-02-22 13:44:39 +0100

    Finish chapter 10 and 11

---
+++ Chapter10/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+namespace Chapter10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////.Load tworzy drzewo X_DOM z pliku, URL, obiektów typu Stream, TetReader i XmlReader
+            ////.Parse tworzy z łańcucha
+            ////XElement config = XElement.Parse(@"<configuration><client enabled = 'true'> <timeout>30</timeout></client></configuration>");
+            //XElement config = XElement.Load("config.xml"); 
+
+            //XElement client = config.Element("client");
+            //bool enabled = (bool)client.Attribute("enabled");//prawda
+            //client.Attribute("enabled").SetValue(!enabled);
+            ////client.Add(new XElement("retries", 3));
+            //Console.WriteLine(config);
+            ////Console.WriteLine(config.ToString(SaveOptions.DisableFormatting));
+            ////config.Save("config.xml");//zapis do pliku
+
+
+
+            //XElement lastName = new XElement("lastname", "Kowalski");
+            //lastName.Add(new XComment("fajne nazwisko"));
+            //XElement customer = new XElement("customer");
+            //customer.Add(new XAttribute("id", 123));
+            //customer.Add(new XElement("firstname", "Jan"));
+            //customer.Add(lastName);
+            //Console.WriteLine(customer.ToString());
+
+            ////konstrukcja funkcyjna
+            //XElement customer = new XElement("customer",
+            //                        new XAttribute("id", 123),
+            //                        new XElement("firstname", "jan"),
+            //                        new XElement("lastname", "kowalski",
+            //                            new XComment("fajne nazwisko")
+            //                        )
+            //);
+            //Console.WriteLine(customer.ToString());
+
+            //var bench = new XElement("bench", 
+            //    new XElement("toolbox", new XElement("handtool", "Młotek"), new XElement("handtool", "tarnik")),
+            //    new XElement("toolbox", new XElement("handtool", "Piłą"), new XElement("powertool", "pistolet na goździe")),
+            //    new XComment("Ostrożnie z pistoletem na gwożdzie")
+            //    );
+            //foreach (XNode node in bench.Nodes())
+            //    Console.WriteLine(node.ToString(SaveOptions.DisableFormatting) + ".");
+            //foreach (XElement e in bench.Elements())//zwraca tylko elementy potomne
+            //    Console.WriteLine(e.Name + " = " + e.Value);
+            //IEnumerable<string> query = from n in bench.Elements()
+            //                            where n.Elements().Any(a => a.Value == "tarnik")
+            //                            select n.Value;
+            //foreach (string i in query)
+            //    Console.WriteLine(i);
+            //IEnumerable<string> query2 = from n in bench.Elements()
+            //                             from a in n.Elements()
+            //                             where a.Name == "handtool"
+            //                             select a.Value;
+            //foreach (string i in query2)
+            //    Console.WriteLine(i);
+            //IEnumerable<String> query3 = from n in bench.Elements("toolbox").Elements("handtool")
+            //                             select n.Value.ToUpper();
+            //foreach (string i in query3)
+            //    Console.WriteLine(i);
+            //Console.WriteLine(bench.Descendants("handtool").Count() + Environment.NewLine);
+
+            //IEnumerable<string> query4 = from c in bench.DescendantNodes().OfType<XComment>()
+            //                             where c.Value.Contains("Ostrożnie")
+            //                             orderby c.Value
+            //                             select c.Value;
+            //foreach (string i in query4)
+            //    Console.WriteLine(i);
+
+            XElement project = XElement.Load("../../Chapter10.csproj");
+            XNamespace ns = project.Name.Namespace;
+            var query = new XElement("ProjectReport",
+                from compileItem in project.Elements(ns + "ItemGroup").Elements(ns + "Compile")
+                let include = compileItem.Attribute("Include")
+                where include != null
+                select new XElement("File", include.Value)
+                );
+            Console.WriteLine(query);
+
+            Console.WriteLine(Environment.NewLine);
+            IEnumerable<string> paths = from compileItem in project.Elements(ns + "ItemGroup").Elements(ns + "Compile")
+                                        let include = compileItem.Attribute("Include")
+                                        where include != null
+                                        select include.Value;
+            var query2 = new XElement("Project", ExpandPaths(paths));
+            Console.WriteLine(query2);
+
+
+            //XmlReaderSettings settings = new XmlReaderSettings();
+            //settings.IgnoreWhitespace = true;
+            //using (XmlReader r = XmlReader.Create("logfile.xml", settings))
+            //{
+
+            //}
+
+
+
+                Console.ReadKey();
+        }
+        static IEnumerable<XElement> ExpandPaths(IEnumerable<string> paths)
+        {
+            var brokenUp = from path in paths
+                           let split = path.Split(new char[] { '\\' }, 2)
+                           orderby split[0]
+                           select new
+                           {
+                               name = split[0],
+                               remainder = split.ElementAtOrDefault(1)
+                           };
+            IEnumerable<XElement> files = from b in brokenUp
+                                          where b.remainder == null
+                                          select new XElement("file", b.name);
+            IEnumerable<XElement> folders = from b in brokenUp
+                                            where b.remainder != null
+                                            group b.remainder by b.name into grp
+                                            select new XElement("folder",
+                                                new XAttribute("name", grp.Key),
+                                                ExpandPaths(grp)
+                                            );
+            return files.Concat(folders);
+        }
+    }
+}
---
+++ Chapter11/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Chapter11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReaderSettings r_settings = new XmlReaderSettings();
+            r_settings.IgnoreComments = true;
+            r_settings.IgnoreWhitespace = true;
+
+
+            XmlWriterSettings w_settings = new XmlWriterSettings();
+            w_settings.Indent = true;//ustawia wcięcia
+
+            Contacts abc = new Contacts();
+            abc.Suppliers.Add(new Supplier() { Name = "Dostawca" });
+            abc.Customers.Add(new Customer() { firstName = "Piotr", lastName = "Smuga", ID = 1});
+            abc.Customers.Add(new Customer() { firstName = "Marcin", lastName = "Smuga", ID = 2 });
+            abc.Customers.Add(new Customer() { firstName = "Dawid", lastName = "Smuga" });
+
+            //using (XmlWriter writer = XmlWriter.Create("..\\..\\foo.xml", w_settings))
+            //{
+            //    writer.WriteStartElement("contacts");
+            //    abc.WriteXml(writer);
+            //    writer.WriteEndElement();
+            //}
+
+            //Contacts cba;
+            //using (XmlReader reader = XmlReader.Create("..\\..\\foo.xml", r_settings))
+            //{
+            //    cba = new Contacts(reader);
+
+            //}
+            //Console.WriteLine(cba.ToString());
+            //Console.WriteLine("End");
+
+
+            XElement test = XElement.Load("..\\..\\foo.xml");
+            Console.WriteLine(test);
+            Console.WriteLine(Environment.NewLine);
+            //using (XmlReader r = XmlReader.Create("..\\..\\foo.xml", r_settings))
+            //{
+            //    while(r.Read())
+            //    {
+            //        Console.Write(new string(' ', r.Depth * 2));
+            //        Console.WriteLine(r.NodeType);
+            //    }
+            //}
+                Console.WriteLine(Environment.NewLine);
+            var query = from n in test.Elements("customer")
+                        where n.Attributes().Any(a => a.Name == "id")
+                        from ala in n.Elements("firstname")                               
+                        select ala.Value;      //wyswietla tylko imina tych co maja id z drzewa                    
+            foreach (var i in query)
+                Console.WriteLine(i);
+
+            //konstrukcja funkcyjna
+            Customer[] ak = { new Customer(), new Customer() };
+            XElement q = new XElement("customers",
+                from c in ak
+                select
+                    new XElement("customer",c.ID  == null ? null: new XAttribute("id", c.ID),
+                        new XElement("firstname", c.firstName),
+                        new XElement("lastname", c.lastName,
+                            new XComment("fajne nazwisko"))
+                    )
+                );
+            q.AddAnnotation("Witaj");//lepiej umiwescic obiekt własnej klasy
+            q.AddAnnotation(new CustomData { Message = "Hello World!" });
+            Console.WriteLine(q.ToString());
+            Console.WriteLine(q.Annotation<string>());//Witaj
+            Console.WriteLine(q.Annotations<CustomData>().Last().Message);//Hello world
+
+            //XText jak się tworzy to jako wartosc dodaja stringi zamiast nadpisywac
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+    class CustomData
+    {
+        internal string Message;
+    }
+
+    public class Contacts
+    {
+        public IList<Customer> Customers = new List<Customer>();
+        public IList<Supplier> Suppliers = new List<Supplier>();
+        public Contacts() { }
+        public Contacts(XmlReader r)
+        {
+            ReadXml(r);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Klienci:");
+            foreach(Customer c in Customers)
+            {
+                Console.WriteLine("\t" + c.ToString());
+            }
+            Console.WriteLine("Dostawcy:");
+            foreach (Supplier s in Suppliers)
+            {
+                Console.WriteLine("\t" + s.ToString());
+            }
+            return "";
+        }
+        public void ReadXml(XmlReader r)
+        {
+            bool isEmpty = r.IsEmptyElement;
+            r.ReadStartElement();
+            if (isEmpty) return;
+            while(r.NodeType == XmlNodeType.Element)
+            {
+                if (r.Name == Customer.XmlName)
+                    Customers.Add(new Customer(r));
+                else if (r.Name == Supplier.XmlName)
+                    Suppliers.Add(new Supplier(r));
+                else
+                    throw new XmlException("Nieoczekiwany węzeł: " + r.Name);
+            }
+            r.ReadEndElement();
+        }
+        public void WriteXml(XmlWriter w)
+        {
+            foreach(Customer c in Customers)
+            {
+                w.WriteStartElement(Customer.XmlName);
+                c.WriteXml(w);
+                w.WriteEndElement();
+            }
+            foreach(Supplier s in Suppliers)
+            {
+                w.WriteStartElement(Supplier.XmlName);
+                s.WriteXml(w);
+                w.WriteEndElement();
+            }
+        }
+    }
+    public class Customer
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public static  string XmlName = "customer";
+        public int? ID;
+        public Customer() {
+            firstName = "";
+            lastName = "";
+            //ID = 1;
+        }
+        public Customer(XmlReader r)
+        {
+            ReadXml(r);
+        }
+        public override string ToString()
+        {
+            string help = "";
+            if(ID.HasValue)
+            help = " Id: " + ID;
+            return firstName + " " + lastName + help; ;
+        }
+        public void ReadXml(XmlReader r)
+        {
+            if (r.MoveToAttribute("id"))
+                ID = r.ReadContentAsInt();
+            r.ReadStartElement();
+            firstName = r.ReadElementContentAsString("firstname", "");
+            lastName = r.ReadElementContentAsString("lastname", "");
+            r.ReadEndElement();
+        }
+        public void WriteXml(XmlWriter w)
+        {
+            if (ID.HasValue) w.WriteAttributeString("id", "", ID.ToString());
+            w.WriteElementString("firstname", firstName);
+            w.WriteElementString("lastname", lastName);
+        }
+    }
+    public class Supplier
+    {
+        public string Name { get; set; }
+        public static string XmlName = "supplier";
+        public Supplier() { }
+        public Supplier(XmlReader r)
+        {
+            ReadXml(r);
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+        public void ReadXml(XmlReader r)
+        {
+            r.ReadStartElement();
+            Name = r.ReadElementContentAsString("name", "");
+            r.ReadEndElement();
+        }
+        public void WriteXml(XmlWriter w)
+        {
+            w.WriteElementString("name", Name);
+        }
+    }
+}
