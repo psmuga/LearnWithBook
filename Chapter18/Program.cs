195e058613f029fdbd43b2ee709f61948d2e59a3
Piotr Smuga <smuga.piotr@hotmail.com>
2017-07-10 10:38:43 +0200

    Finish Chapter 18

---
+++ Chapter18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(System.Threading.Thread.CurrentThread.CurrentCulture);
+            Console.WriteLine(System.Threading.Thread.CurrentThread.CurrentUICulture);
+
+            //aktualnie wczytane w pamięci podzespoły
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                Console.WriteLine(assembly.Location);
+                Console.WriteLine(assembly.CodeBase);
+                Console.WriteLine(assembly.GetName().Name);
+            }
+
+            //Assembly a = Assembly.LoadFrom(@"c:\temp\lib.dll");
+
+            //http://www.albahari.com/nutshell/cs4ch17.aspx
+            Console.ReadKey();
+        }
+    }
+}
